c1fb45a57b15758a139735d0d02fc072712ca98f
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-20 22:28:56 +0600

    modify app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-
+import eventRoutes from "./routes/event.route";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/events", eventRoutes);
 
 export default app;
