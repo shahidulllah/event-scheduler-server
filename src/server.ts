38a7cb73cd3170da81f8e2d9a9eeee511fc9144e
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-20 22:37:54 +0600

    env fix

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import app from './app';
+import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
