86468b67659370a33557c73837a979fcbb59887f
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-22 02:20:20 +0600

    working on just modifying

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import eventRoutes from "./routes/event.route";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://event-scheduler-alpha.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/events", eventRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to my Mini Event Scheduler");
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.use
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
