7004b65543ae77bba8f779de89d53d8f73bad69f
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-30 22:14:14 +0600

    nothing

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000;
 
 app.use;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Event Scheduler Server is running on port ${PORT}`);
 });
