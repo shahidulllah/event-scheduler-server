1505d83a154e3d5f708cff2b25fcb662df4cc99b
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-01 21:10:33 +0600

    adding comment

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ app.use(
 app.use(express.json());
 app.use("/events", eventRoutes);
 
+//Checking and running
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to my Mini Event Scheduler");
 });
